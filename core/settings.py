1c56ca9b02cc0f4e411d91d1833f9da7276e86dc
Dominic Plouffe <dominic@dplouffe.ca>
2024-11-17 13:39:18 -0500

    added env file

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+IS_PROD = os.environ.get("ENV", "local").lower() == "prod"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
