7b94428786378e1002558ceebfa364f28e75200f
Dominic Plouffe <dominic@dplouffe.ca>
2024-11-17 21:59:03 -0500

    database scheduler test

--- core/settings.py
+++ core/settings.py
@@ -200,7 +200,7 @@ CELERY_BEAT_SCHEDULE = {
     },
     "zimuabull.tasks.scan.scan": {
         "task": "zimuabull.tasks.scan.scan",
-        "schedule": crontab(hour=2, minute=0),
+        "schedule": crontab(hour=3, minute=5),
         "options": {"queue": "pidashtasks"},
     },
 }
